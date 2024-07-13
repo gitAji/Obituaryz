9ef356106131f0395cd298bc123e760da5759bb6
gitAji <89026824+gitAji@users.noreply.github.com>
2024-07-14 00:27:59 +0200

    tamil added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const sendCondolences = () => {
@@ -7,6 +7,14 @@ const sendCondolences = () => {
 };
 
 const App = () => {
+  const [language, setLanguage] = useState("en"); // Default language is English
+
+  // Function to toggle between English and Tamil
+  const toggleLanguage = () => {
+    const newLanguage = language === "en" ? "ta" : "en"; // Toggle between English ("en") and Tamil ("ta")
+    setLanguage(newLanguage);
+  };
+
   useEffect(() => {
     const createFlower = () => {
       const flower = document.createElement("div");
@@ -27,7 +35,21 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1 className="main-heading">Obituary Notice</h1>
+      {/* Language toggle button */}
+      <div className="language-btn">
+        <div
+          className={`language-toggle ${
+            language === "en" ? "english" : "tamil"
+          }`}
+          onClick={toggleLanguage}
+        >
+          {language === "en" ? "Switch to Tamil" : "ஆங்கிலத்தில் மாற்று"}
+        </div>{" "}
+      </div>
+
+      <h1 className="main-heading">
+        {language === "en" ? "Obituary Notice" : "மரண அறிவித்தல்"}
+      </h1>
       <div className="obituary-card">
         <div className="image-section">
           <img src="/lamp.png" alt="Lamp" className="side-lamp" />
@@ -41,50 +63,62 @@ const App = () => {
           <img src="/lamp.png" alt="Lamp" className="side-lamp" />
         </div>
         <h3 className="occasion-subtitle">
-          The late Mr. Kanapathipillai Kanesan
+          {language === "en"
+            ? "The late Mr. Kanapathipillai Kanesan"
+            : "அமரர் திரு. கணபதிப்பிள்ளை கணேசன்"}
         </h3>
         <p className="obituary-dates">
-          Born in Kapar, Malaysia - Passed away on Friday, July 12, 2024
+          {language === "en"
+            ? "Born in Kapar, Malaysia - Passed away on Friday, July 12, 2024"
+            : "மலேசியா காப்பாறை (Kapar) பிறப்பிடமாகவும், வட்டுவத்தை அல்வாலையை வசிப்பிடமாகவும், கொண்ட "}
         </p>
 
         <p className="obituary-text">
-          The late Mr. Kanapathipillai Kanesan, a retired manager (co-op
-          Nelliady), was born in Kapar, Malaysia, and resided in Vatuwattai
-          Alwaai(Point Petro). He passed away on Friday, July 12, 2024.
-        </p>
-        <p className="obituary-text">
-          He is eldest son of the late Kanapathipillai Maheshwari and dear
-          son-in-law of the late Mr. Visivalingam, he was the beloved husband of
-          Thiriphuvaneshwari(Vevi).
-        </p>
-        <p className="obituary-text">
-          He was a dear brother to Parvathidevi, Subramaniam, Sirbalakrishnan,
-          Kumarasamy, Deivanayake, Navaneethakrishnan, and Parameshwari.
-        </p>
-        <p className="obituary-text">
-          He was also the brother-in-law of Sivagangai, the late Chelladurai,
-          Parvathavarthini, Arulmolirani, the late Sivashankaranathan, the late
-          Jenaranjini, and Kugathasan.
-        </p>
-        <p className="obituary-text">
-          Beloved by the late Sri Balaganesamurthy, the late Sivapakyam, and
-          Thilakavathy, he was also the brother-in-law of Padmalokini,
-          Sivaprakasam, and Singha Mappanar.
-        </p>
-        <p className="obituary-text">
-          Annar's last cremation rites will be held on Sunday, July 14, 2024, at
-          10:00 AM at his residence, with the cremation taking place at
-          Karukamban (Near Peru Moodi Madam) Hindu Cemetery.
-        </p>
-        <p className="obituary-text">
-          We kindly request all relatives and friends to accept this
-          announcement.
-        </p>
-        <p className="obituary-text">
-          For further information, please contact: Thiriphuvaneshwari (Wife)
-          Tel:0094773867798 Viber:009475043797
+          {language === "en"
+            ? "The late Mr. Kanapathipillai Kanesan, a retired manager (co-op Nelliady), was born in Kapar, Malaysia, and resided in Vatuwattai Alwaai (Point Petro). He passed away on Friday, July 12, 2024."
+            : "திரு கணபதிப்பிள்ளை கணேசன் ஓய்வு பெற்ற முகாமையாளர் (ப.நோ.கூ சங்கம் நெல்லியடி) அவர்கள் 12-07-2024 வெள்ளிக்கிழமை அன்று காலமானார்."}
         </p>
 
+        <p className="obituary-text">
+          {language === "en"
+            ? "He is the eldest son of the late Kanapathipillai Maheshwari and dear son-in-law of the late Mr. Visivalingam, and the beloved husband of Thiriphuvaneshwari (Vevi)."
+            : "அன்னார், காலஞ்சென்றவர்களான திரு/திருமதி கணபதிப்பிள்ளை மகேஸ்வரி, தம்பதிகளின் சிரேஷ்ட  புத்திரனும், காலம் சென்றவர்களான திரு விசிவலிங்கம் தம்பதிகளின்  பாசமிகு மருமகனும்.திரிபுவனேஸ்வரி அவர்களின்   அன்புக் கணவரும்."}
+        </p>
+
+        <p className="obituary-text">
+          {language === "en"
+            ? "He was a dear brother to Parvathidevi, Subramaniam, Sirbalakrishnan, Kumarasamy, Deivanayake, Navaneethakrishnan, and Parameshwari."
+            : "அவர்கள் பார்வதிதேவி,  சுப்பிரமணியம், பாலகிருஷ்ணன், குமாரசாமி, தெய்வநாயகி, நவநீதகிருஷ்ணன், பரமேஸ்வரி  ஆகியோரின் அன்புச்  சகோதரரும்."}
+        </p>
+
+        <p className="obituary-text">
+          {language === "en"
+            ? "He was also the brother-in-law of Sivagangai, the late Chelladurai, Parvathavarthini, Arulmolirani, the late Sivashankaranathan, the late Jenaranjini, and Kugathasan."
+            : "அவர்கள் சிவகங்கை, காலம் சென்ற செல்லதுரை,  பார்வதவர்த்தினி, அருள்மொழிராணி, சிவசங்கரநாதன், காலம் சென்ற ஜெனரஞ்சினி,மற்றும் குகதாசன்  ஆகியோரின்  மைத்துணரும்,"}
+        </p>
+
+        <p className="obituary-text">
+          {language === "en"
+            ? "Beloved by the late Sri Balaganesamurthy, the late Sivapakyam, and Thilakavathy, he was also the brother-in-law of Padmalokini, Sivaprakasam, and Singha Mappanar."
+            : "அவர் காலம் சென்ற ஸ்ரீ பாலகணேசமூர்த்தி, காலம் சென்ற சிவபாக்கியம், மற்றும் திலகவதி ஆகியோரின்  பாசமிகு  அத்தானும்,  பத்மலோகினி, சிவப்பிரகாசம், சிங்க மாப்பாணர்  ஆகியோரின் மைத்துணருமாவார்."}
+        </p>
+
+        <p className="obituary-text">
+          {language === "en"
+            ? "Annar's last cremation rites will be held on Sunday, July 14, 2024, at 10:00 AM at his residence, with the cremation taking place at Karukamban (Near Peru Moodi Madam) Hindu Cemetery."
+            : "அன்னாரின் இறுதிக்கிரியை 14-07-2024 ஞாயிற்றுக்கிழமை அன்று மு.ப 10:00 மணியளவில் அவரது இல்லத்தில் நடைபெற்று பின்னர் மடத்தடி   கருகம்பன்( பெரு மூடி மடத்துக்கு அருகாமையில்) இந்து மயானத்தில் பூதவுடல் தகனம் செய்யப்படும்."}
+        </p>
+
+        <p className="obituary-text">
+          {language === "en"
+            ? "We kindly request all relatives and friends to accept this announcement."
+            : "இவ் அறிவித்தலை உற்றார்,உறவினர், நண்பர்கள் அனைவரும் ஏற்றுக்கொள்ளுமாறு கேட்டுக்கொள்கின்றோம்."}
+        </p>
+        <p className="obituary-text">
+          {language === "en"
+            ? "For further information, please contact: Thiriphuvaneshwari (Wife) Tel:0094773867798 Viber:009475043797"
+            : "தகவல் குடும்பத்தினர் || தொடர்புகளுக்கு:திரிபுவனேஸ்வரி (மனைவி) கைபேசி:0094773867798 வைபர்:009475043797"}
+        </p>
         <div className="buttons">
           <a
             className="live-button"
@@ -92,7 +126,7 @@ const App = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Live Stream
+            {language === "en" ? "Live Stream" : "நேரடி ஒளிபரப்பு"}
           </a>
 
           <a
@@ -101,7 +135,7 @@ const App = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Call
+            {language === "en" ? "Call" : "அழை"}
           </a>
 
           <a
@@ -110,11 +144,11 @@ const App = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Viber Call
+            {language === "en" ? "Viber Call" : "வைபர் அழைப்பு"}
           </a>
 
           <button className="condolences-button" onClick={sendCondolences}>
-            Send Condolences
+            {language === "en" ? "Send Condolences" : "அனுதாபங்களை அனுப்ப"}
           </button>
         </div>
       </div>
