72c7b802304cc7f9788928b94c9e42eca81530b9
gitAji <89026824+gitAji@users.noreply.github.com>
2024-07-14 00:45:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 const sendCondolences = () => {
